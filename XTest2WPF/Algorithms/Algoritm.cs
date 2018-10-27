96589a7b16366930da1c14021f37ef09184982f1
Tiurin Valery <tiurinvalery@gmail.com>
2018-10-27 19:54:38 +0300

    Add basic interface for Algoritm

---
+++ XTest2WPF/Algorithms/Algoritm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTest2WPF.Algorithms
+{
+    interface Algoritm
+    {
+        AlgoritmDTO code(AlgoritmDTO dto);
+
+        AlgoritmDTO decode(AlgoritmDTO dto);
+    }
+}
---
+++ XTest2WPF/Algorithms/AlgoritmDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTest2WPF.Algorithms
+{
+    class AlgoritmDTO
+    {
+        private String request { get; set; }
+        private String response { get; set; }
+        private String error { get; set; }
+        private List<int> indexErrors { get; set; }
+    }
+}
